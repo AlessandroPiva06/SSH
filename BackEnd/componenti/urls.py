84a680b74d692cf2709e459558119f79c9c9b8b2
FaustoDeIaco <fausto.deiaco@fermi.mo.it>
2026-04-28 12:47:44 +0200

    dioporco odio fausto

--- BackEnd/componenti/urls.py
+++ BackEnd/componenti/urls.py
@@ -1,12 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path
 from .views import ComponenteViewSet, FamigliaViewSet, TagViewSet, LocazioneViewSet
+
 router = DefaultRouter()
 router.register(r'famiglie', FamigliaViewSet)
 router.register(r'tag', TagViewSet)
 router.register(r'locazioni', LocazioneViewSet)
 router.register(r'componenti', ComponenteViewSet)
 
-urlpatterns = router.urls + [
-    path('product/', product_page, name='product-page'),
-]
+urlpatterns = router.urls
