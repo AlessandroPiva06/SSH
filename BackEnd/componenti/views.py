31acd28f5cf844ef7212bcc45a9445286ed64337
AlessandroPiva <alessandro.piva@fermi.mo.it>
2026-05-05 12:11:12 +0200

    aggiunta la possibilità di modificare direttamente da sito

--- BackEnd/componenti/views.py
+++ BackEnd/componenti/views.py
@@ -8,30 +8,26 @@ from .models import Componente, Famiglia, Tag, Locazione
 from .serializers import ComponenteSerializer, FamigliaSerializer, TagSerializer, LocazioneSerializer
 
 class PermessoPerRuolo(permissions.BasePermission):
+    """
+    Tutti gli utenti approvati possono leggere E scrivere.
+    Gli utenti non approvati non hanno accesso.
+    """
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        if request.method in permissions.SAFE_METHODS:
-            return request.user.approvato
-        return request.user.ruolo in ['admin', 'tecnico']
+        return request.user.approvato
 
 class ComponenteViewSet(viewsets.ModelViewSet):
     queryset = Componente.objects.all()
     serializer_class = ComponenteSerializer
     permission_classes = [PermessoPerRuolo]
 
-    # Filtering per campi esatti
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['famiglia', 'tag', 'locazione', 'is_scorta']
-
-    # Ricerca testuale
     search_fields = ['nome', 'famiglia__nome', 'tag__nome']
-
-    # Ordinamento
     ordering_fields = ['nome', 'quantita', 'quantita_minima']
-    ordering = ['nome']  # default
+    ordering = ['nome']
 
-    # Endpoint extra: /api/componenti/sotto_scorta/
     @action(detail=False, methods=['get'])
     def sotto_scorta(self, request):
         componenti = Componente.objects.filter(quantita__lte=models.F('quantita_minima'))
