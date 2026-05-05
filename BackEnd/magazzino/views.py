5fbdf1975d8aa438e1dcdf481bb1b386830b354e
Fausto De Iaco <fausto.deiaco@fermi.mo.it>
2026-05-05 11:51:15 +0200

    tolto permesso ai prof di aggiungere componenti

--- BackEnd/magazzino/views.py
+++ BackEnd/magazzino/views.py
@@ -1,7 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Movimento
 from .serializers import MovimentoSerializer
+
+class PermessoMagazzino(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        # Solo admin e tecnico possono fare carico/scarico
+        return request.user.ruolo in ['admin', 'tecnico']
 
 class MovimentoViewSet(viewsets.ModelViewSet):
     queryset = Movimento.objects.all()
     serializer_class = MovimentoSerializer
+    permission_classes = [PermessoMagazzino]
