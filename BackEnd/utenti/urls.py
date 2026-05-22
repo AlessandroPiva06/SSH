e208b579c455fb170614f20093de35c34f55701c
AlessandroPiva <alessandro.piva@fermi.mo.it>
2026-05-22 02:20:54 +0200

    versione finale

--- BackEnd/utenti/urls.py
+++ BackEnd/utenti/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegistrazioneView, ProfiloView, ListaUtentiView, ApprovaUtenteView
+from .views import RegistrazioneView, ProfiloView, ListaUtentiView, ApprovaUtenteView, LogView
 
 urlpatterns = [
     path('registrazione/', RegistrazioneView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),        # restituisce access + refresh token
-    path('token/refresh/', TokenRefreshView.as_view()),   # rinnova l'access token
+    path('login/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('profilo/', ProfiloView.as_view()),
     path('utenti/', ListaUtentiView.as_view()),
     path('utenti/<int:pk>/approva/', ApprovaUtenteView.as_view()),
+    path('log/', LogView.as_view()),
 ]
